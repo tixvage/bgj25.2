4f7ede85b2866b3b6a45db4681ee5682baa1947f
melih <mel10h3421@gmail.com>
2025-08-24 16:19:20 +0300

    add accel

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,6 +2,14 @@ extends CharacterBody2D
 
 @export var player_datas: Array[PlayerData]
 
+@export var ground_accel := 2500.0
+@export var air_accel := 1800.0
+@export var coyote_time := 0.15
+
+const DASH_FORCE: float = 90.0
+const DASH_LIMIT: float = 150.0
+const SHAKE_FORCE: float = 1.0
+
 @onready var camera: Camera2D = %Camera2D
 @onready var sprite: Sprite2D = $Sprite2D
 @onready var dash_ray: RayCast2D = $DashRay
@@ -9,13 +17,10 @@ extends CharacterBody2D
 
 @onready var player_ghost_scene := preload("res://scenes/player_ghost.tscn")
 
+var current_data: int = 0
 var is_dashing: bool = false
 var dash_distance: float = 0.0
-var current_data: int = 0
-
-const DASH_FORCE: float = 90.0
-const DASH_LIMIT: float = 150.0
-const SHAKE_FORCE: float = 1
+var coyote_timer: float = 0.0
 
 func _ready() -> void:
 	Global.camera_manager.camera = camera
@@ -27,22 +32,30 @@ func get_current_data() -> PlayerData:
 func _physics_process(delta: float) -> void:
 	var data := get_current_data()
 
-	if not is_on_floor():
+	var on_floor := is_on_floor()
+	var can_jump: bool = coyote_timer > 0.0
+	if on_floor:
+		coyote_timer = coyote_time
+	else:
+		coyote_timer = max(coyote_timer - delta, 0.0)
+	var can_dash: bool = not on_floor and not dash_ray.is_colliding()
+	var accel = ground_accel if on_floor else air_accel
+
+
+	if not on_floor:
 		velocity += get_gravity() * delta
 
-	if Input.is_action_just_pressed("move_down") and not is_on_floor():
-		var can_dash: bool = not dash_ray.is_colliding()
-		if can_dash:
-			is_dashing = true
-			dash_distance = position.y
-			ghost_spawn_timer.one_shot = false
-			ghost_spawn_timer.start()
-			if velocity.y < 0.0:
-				velocity.y = DASH_FORCE * data.mass
-			else:
-				velocity.y += DASH_FORCE * data.mass
+	if Input.is_action_just_pressed("move_down") and can_dash:
+		is_dashing = true
+		dash_distance = position.y
+		ghost_spawn_timer.one_shot = false
+		ghost_spawn_timer.start()
+		if velocity.y < 0.0:
+			velocity.y = DASH_FORCE * data.mass
+		else:
+			velocity.y += DASH_FORCE * data.mass
 
-	if is_on_floor() and is_dashing:
+	if on_floor and is_dashing:
 		is_dashing = false
 		dash_distance = position.y - dash_distance
 		ghost_spawn_timer.one_shot = true
@@ -50,14 +63,11 @@ func _physics_process(delta: float) -> void:
 		dash_distance = 0.0
 		Global.camera_manager.shake(SHAKE_FORCE * data.mass * dash_power, 5)
 
-	if Input.is_action_just_pressed("move_up") and is_on_floor():
+	if Input.is_action_just_pressed("move_up") and can_jump:
 		velocity.y = -data.jump_force
 
 	var direction := Input.get_axis("move_left", "move_right") if not is_dashing else 0.0
-	if direction:
-		velocity.x = direction * data.move_speed
-	else:
-		velocity.x = move_toward(velocity.x, 0, data.move_speed)
+	velocity.x = move_toward(velocity.x, direction * data.move_speed, accel * delta)
 
 	move_and_slide()
 
@@ -65,5 +75,4 @@ func _physics_process(delta: float) -> void:
 func _on_ghost_spawn_timer_timeout() -> void:
 	var ghost_instance = player_ghost_scene.instantiate()
 	ghost_instance.apply_sprite(sprite, position)
-	print("hellow")
 	get_parent().add_child(ghost_instance)
