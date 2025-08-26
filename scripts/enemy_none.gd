29495d609cd6c87b232fe6ab0a9aee363fb36064
melih <melihbykr00@gmail.com>
2025-08-26 10:20:19 +0300

    mfs can jump now

--- scripts/enemy_none.gd
+++ scripts/enemy_none.gd
@@ -4,6 +4,7 @@ extends CharacterBody2D
 
 enum State {
 	RANDOM,
+	JUMP,
 	KNOCKBACK,
 }
 
@@ -18,17 +19,21 @@ var current_target_x: float
 var knockback: Vector2
 var knockback_timer: float = 0.0
 var health: float = MAX_HEALTH
+var need_new_target: bool = false
+var skipped_possible_jump: bool = false
 
 @onready var flash_timer: Timer = $FlashTimer
 @onready var sprite: Sprite2D = $Root/Sprite2D
-@onready var direction_ray: RayCast2D = $Root/DirectionRay
+@onready var obstacle_ray: RayCast2D = $Root/ObstacleRay
+@onready var optional_jump_ray: RayCast2D = $Root/OptionalJumpRay
+@onready var head_ray: RayCast2D = $Root/HeadRay
 @onready var root: Node2D = $Root
 
 func change_state(new_state: State) -> void:
 	state = new_state
-	if state == State.RANDOM:
+	if state == State.RANDOM and need_new_target:
 		current_target_x = randf_range(Global.enemy_manager.world_limit_min, Global.enemy_manager.world_limit_max)
-
+		need_new_target = false
 
 func _ready() -> void:
 	change_state(State.RANDOM)
@@ -75,10 +80,25 @@ func _physics_process(delta: float) -> void:
 
 	if state == State.RANDOM:
 		if abs(global_position.x - current_target_x) < Global.EPS:
+			need_new_target = true
 			change_state(State.RANDOM)
 		velocity.x = global_position.direction_to(Vector2(current_target_x, global_position.y)).x * 200.0
-		if direction_ray.is_colliding() and is_on_floor():
+		if obstacle_ray.is_colliding() and is_on_floor():
 			velocity.y = -300
+		
+		#worst shit
+		if skipped_possible_jump and head_ray.is_colliding():
+			skipped_possible_jump = false
+		if optional_jump_ray.is_colliding() and is_on_floor() and not head_ray.is_colliding():
+			if not skipped_possible_jump:
+				if Global.rng.randi() % 2 == 1:
+					change_state(State.JUMP)
+				else:
+					skipped_possible_jump = true
+	elif state == State.JUMP:
+		velocity.y = -500
+		if not optional_jump_ray.is_colliding() or head_ray.is_colliding():
+			change_state(State.RANDOM)
 	elif state == State.KNOCKBACK:
 		if knockback_timer > 0.0:
 			knockback_timer -= delta
